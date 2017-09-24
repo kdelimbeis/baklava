f34fbea1207f1ab82df3c12b6e00ab68a860936e
kostas delimbeis <kdelimbeis@comcast.net>
2017-09-24 11:03:01 -0400

    about

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'about/index'
   root 'welcome#index'
 
   devise_for :users
   resources :users
+  resources :menus
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170922190805) do
+ActiveRecord::Schema.define(version: 20170923190014) do
+
+  create_table "menus", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
